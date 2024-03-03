2299269893dcf6c8df1fb4426780f185dbdf28ca
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-03-03 08:25:30 -0500

    chore: indents

--- init.zsh
+++ init.zsh
@@ -25,24 +25,24 @@ p6df::modules::p6helm::deps() {
 ######################################################################
 p6_helm_prompt_info() {
 
-    local str
+  local str
 
-    if ! p6_string_blank "$KUBECONFIG"; then
-      str="helm:     "
-    fi
+  if ! p6_string_blank "$KUBECONFIG"; then
+    str="helm:     "
+  fi
 
-    local helm_ctx=${HELM_KUBECONTEXT:-$P6_KUBE_CFG}
-    if ! p6_string_blank "$helm_ctx"; then
-      str="${str}ctx=[$helm_ctx]"
-    fi
-    local helm_ns=${HELM_NAMESPACE:-$P6_KUBE_NS} 
-    if ! p6_string_blank "$helm_ns"; then
-      str="${str} ns=[$helm_ns]"
-    fi
+  local helm_ctx=${HELM_KUBECONTEXT:-$P6_KUBE_CFG}
+  if ! p6_string_blank "$helm_ctx"; then
+    str="${str}ctx=[$helm_ctx]"
+  fi
+  local helm_ns=${HELM_NAMESPACE:-$P6_KUBE_NS}
+  if ! p6_string_blank "$helm_ns"; then
+    str="${str} ns=[$helm_ns]"
+  fi
 
-    if p6_string_blank "$str"; then
-      p6_return_void
-    else
-      p6_return_str "$str"
-    fi
+  if p6_string_blank "$str"; then
+    p6_return_void
+  else
+    p6_return_str "$str"
+  fi
 }
